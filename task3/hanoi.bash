53b94067a440e89e016af3fe14cca912b6ae664b
zaytc145 <zaytsevdanil@yandex.ru>
2021-12-01 23:20:06 +0300

    hanoi

--- task3/hanoi.bash
+++ task3/hanoi.bash
@@ -1,21 +1,63 @@
 #!/bin/bash
-declare -a st1=(8 7 6 5 4 3 2 1)
+declare -a result=(8 7 6 5 4 3 2 1)
+declare -a st_1=(${result[@]})
+declare -a st_2 st_3
+# eval "echo \${st_$num[1]}"
+errorMessage="Неверный ход! Повторите!"
+stecksCount=3
 step=1
-
 while true
 do
-    echo "1) ${st1[1]}"
-    echo "2) ${st2[@]}"
-    echo "3) ${st3[@]}"
+    echo "1) ${st_1[@]}"
+    echo "2) ${st_2[@]}"
+    echo "3) ${st_3[@]}"
 
-    read -pr "Ход № $step (откуда, куда) (q - выход): " -a  inputNums
-    echo ${inputNums[0]}
-    if [ ${inputNum[0]} == 'q' ]
+    if [[ ${st_3[*]} == ${result[*]} ]]
     then
         break
     fi
 
-    # prefix=1
-    # tst=st$prefix
-    # echo "${!tst[@]}"
+    read -p "Ход № $step (откуда, куда) (q - выход): " -a  inputNums
+    
+    if [[ ${inputNums[0]} == "q" ]]
+    then
+        break
+    fi
+
+    inputNumsCount=${#inputNums[@]}
+
+    if [[ ${inputNumsCount} != 2 ]]
+    then
+        echo "$errorMessage"
+        continue
+    fi
+
+    start=${inputNums[0]}
+    target=${inputNums[1]}
+
+    if  ((${inputNums[0]} > $stecksCount)) || ((${inputNums[1]} > $stecksCount ))
+    then
+        echo "$errorMessage"
+        continue
+    fi
+
+    eval "startValue=\${st_$start[@]: -1}"
+    eval "endValue=\${st_$target[@]: -1}"
+
+    if  [[ -z "$startValue" ]]
+    then
+        echo "$errorMessage"
+        continue
+    fi
+
+    if  [[ ! -z "$endValue" ]] && [[ $startValue > $endValue ]]
+    then
+        echo "$errorMessage"
+        continue
+    fi
+
+    eval "st_$target+=(\${st_$start[@]: -1})"
+    eval "unset st_$start[-1]"
+
+    step=$((step+1))
 done
